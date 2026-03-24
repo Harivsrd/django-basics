c3520bc5b6fb455ee6e94c302da5c04f93057b7c
Harivsrd <harivsrdesineedi@gmail.com>
2026-03-24 23:33:51 +0530

    Building the Collections API

--- storefront2/store/views.py
+++ storefront2/store/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.db.models import Count
 from rest_framework import status
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product,Collection
+from .serializers import ProductSerializer , CollectionSerializer
 
 # Create your views here.
 @api_view(['GET' , 'POST'])
@@ -36,6 +37,36 @@ def product_detail(request, id):
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-@api_view()
+
+@api_view(['GET', 'POST'])
+def collection_list(request):
+    if request.method == 'GET':
+        queryset = Collection.objects.annotate(product_count= Count('Products')).all()
+        serializer = CollectionSerializer(queryset, many = True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = CollectionSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data , status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def collection_detail(request, pk):
-    return Response('ok')
+    collection = get_object_or_404(
+        Collection.objects.annotate(
+            product_count=Count('products')),pk=pk)
+    if request.method == 'GET':
+        serializer = CollectionSerializer(collection)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = CollectionSerializer(collection , data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        if collection.products.count() > 0:
+            return Response({'error':'Collection cannot be deleted it includes one or more products'})
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+    
